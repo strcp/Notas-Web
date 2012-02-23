c5f35286201faa5b76ce351ed8be7f8ed983e653
Cristiano Fernandes <cristianobfernandes@gmail.com>
2012-02-23 08:47:34 -0200

    Header

--- ulbra.py
+++ ulbra.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# This file is part of Notas-Web. Notas-Web is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY. See the GNU General Public License for more details
 
 import urllib
 import urllib2
